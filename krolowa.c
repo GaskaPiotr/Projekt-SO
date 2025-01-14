3f5de8a15cedafe29798f743a03712e4baaaa71a
04piotr04@wp.pl <gaska.piotr.151427@torus.uck.pk.edu.pl>
2025-01-14 18:34:42 +0100

    Male zmiany

--- krolowa.c
+++ krolowa.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "my_header.h"
 
@@ -10,8 +13,37 @@ int N = 2;
 int P = 10;
 int number_of_bees_in_hive = 2;
 
+void clean_workers(int sig)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+void spawn_worker(const char *worker_program, const char *worker_arg1, const char *worker_arg2)
+{
+    	pid_t pid = fork();
+
+    	if (pid == -1)
+	{
+        	perror("fork");
+       	 	exit(EXIT_FAILURE);
+    	}
+	else if (pid == 0)
+	{
+        	// Worker process
+        	execlp(worker_program, worker_program, worker_arg1, worker_arg2, NULL);
+       	 	perror("execlp");
+        	exit(EXIT_FAILURE);
+    	}
+    	printf("Spawned worker with PID %d.\n", pid);
+}
+
+
 int main(int argc, char *argv[])
 {
+	const char *worker_program = "./robotnica";
+
+	signal(SIGCHLD, clean_workers);
+
 	if (argc != 2)
 	{
 		printf("./program Tk(number_of_sec_to_lay_eggs)\n");
@@ -36,6 +68,7 @@ int main(int argc, char *argv[])
 				sleep(0.5);
 			}
 			printf("laying eggs\n");
+			spawn_worker(worker_program, "10", "2");
 			//execlp("./robotnica", "./robotnica", "10", "2", NULL);
 			//perror("execlp error");
 			//if (system("./robotnica 10 2") != 0)
